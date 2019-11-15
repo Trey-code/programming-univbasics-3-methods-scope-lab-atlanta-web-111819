efbb28e4932a1f6f04118ff8cf2a4f4fb892d182
Treven Womack <keytonwomack@yahoo.com>
2019-11-15 19:42:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,6 +7,6 @@ def toadstool
   puts status= 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
-def link(phrase)
+def link
   puts phrase = ("It's Dangerous To Go Alone!Take This.")
 end
